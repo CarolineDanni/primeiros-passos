0ef73a25227ed704017ee744062d6625b86b6ef8
Caroline Danni <70079244+CarolineDanni@users.noreply.github.com>
2020-09-28 19:27:35 -0300

    Exercício Aluna Caroline Danni

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,18 @@
+
+
+function checarSePodeDirigir() {
+  let idade = prompt("Qual a sua idade?");
+ 
+ if (idade > 17) {
+    alert("Já pode dirigir!");
+}
+ else {
+    alert("Não pode dirigir ainda!");
+    }
+}
+
+checarSePodeDirigir(18);
+
+/* Rita, não sei se está correto. Tive bastante dificuldade em entender como criar o código em que o Alert de "Não pode dirigir"
+aparecesse sempre. Espero que esteja correto.
+*/
